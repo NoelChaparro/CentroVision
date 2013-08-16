191fee87f7f4bc5138465a1156848ef7099c58ba
joseluisordonez <joseluisordonezmacias@hotmail.com>
2013-08-15 18:37:59 -0600

    jquery y css

---
+++ public/js/navbar.js
@@ -0,0 +1,43 @@
+$(function() {
+	
+	
+	var sections = $("section");
+	var navigation_links = $("nav a");
+	
+	sections.waypoint({
+		handler: function(event, direction) {
+		
+			var active_section;
+			active_section = $(this);
+
+			if (direction === "up") active_section = active_section.prev();
+			var active_link = $('nav a[href="#' + active_section.attr("id") + '"]');
+			navigation_links.removeClass("active");
+			active_link.addClass("active");
+		},
+		offset: '25%'
+	});
+	
+	
+	navigation_links.click( function(event) {
+
+		$.scrollTo(
+			$(this).attr("href"),
+			{
+				duration: 400,
+				offset: { 'left':0, 'top':-0.01*$(window).height() }
+			}
+		);
+	});
+
+		$(".sidebar-nav li").hover( 
+			function(){
+				if ($(this).children().hasClass("") ){
+					$(this).children().children("i").removeClass("icon-white")
+				}
+			},
+			function(){$(this).children().children("i").addClass("icon-white")
+		});
+
+});
+
