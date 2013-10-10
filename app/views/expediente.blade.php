87c791fafce01fdb00668a1d4114630df5745470
joseluisordonez <joseluisordonezmacias@hotmail.com>
2013-10-09 18:41:42 -0600

    cambios imagenes

--- app/views/expediente.blade.php
+++ app/views/expediente.blade.php
@@ -358,18 +358,14 @@
                		<h3>Subir Imagenes</h3>
            		</div>
            		<div class="modal-body">
-           	    	<form id="upload" method="post" action="upload.php" enctype="multipart/form-data">
-						<div id="drop">
-							Arrastre las imagenes aqui
-							<a>Buscar</a>
-							<input type="file" name="upl" multiple />
-						</div>
+           	    	
 
-						<ul>
-							<!-- The file uploads will be shown here -->
-						</ul>
 
-					</form>
+
+
+
+
+
        	    	</div>
  
        		</div>
@@ -1329,13 +1325,4 @@
 	<script src="libs/ckeditor/ckeditor.js"></script>
 	<!-- JavaScript Includes -->
 	
-	<script src="assets/js/jquery.knob.js"></script>
-
-	<!-- jQuery File Upload Dependencies -->
-	<script src="assets/js/jquery.ui.widget.js"></script>
-	<script src="assets/js/jquery.iframe-transport.js"></script>
-	<script src="assets/js/jquery.fileupload.js"></script>
-	
-	<!-- Our main JS file -->
-	<script src="assets/js/script.js"></script>
 @stop
