54538d2f4e7b9e2c1fa2a26ec03e8483675453a5
joseluisordonez <joseluisordonezmacias@hotmail.com>
2013-11-05 19:10:27 -0700

    boton buscar en operacion laser

--- app/views/operacionlaser.blade.php
+++ app/views/operacionlaser.blade.php
@@ -18,9 +18,63 @@
 	</div>
 	<!-- end: Main Menu--> 
 		<div class="offset2">
-			<br><br>
+			</br></br>
 			<section id="lasik">
+			<div class="row offset1">
+					<ul class="hover-block">
+          				<li>
+            				<a href="#mdlBuscarPaciente" role="button" data-toggle="modal" id="btnBuscarPacienteModal">
+				              	<!-- Image -->
+				              	<div class="b-blue cont-icon"><i class="icon-users"></i></div> 
+				              	<!-- Content with background color Class -->
+				              	<div class="hover-content b-blue">
+				                	<h4>Buscar</h4>
+				                	Buscar paciente en la base de datos de acuerdo al nombre
+				              	</div>
+				            </a>
+				        </li>
+					</ul>
+			</div>
+       		<div class="modal hide fade" tabindex="-1" role="dialog" id="mdlBuscarPaciente">
+            	<div class="modal-header">
+               		<a class="close" data-dismiss="modal">&times;</a>
+               		<h3>Buscar Paciente</h3>
+               		<br />
+               		<input type="text" class="input-xlarge search-query span4" id="txtBuscarPaciente" placeholder="Buscar Paciente">
+               		<button type="submit" class="btn" id="btnBuscarPaciente">Buscar</button>
+           		</div>
+           		<div class="modal-body">
+           	    	<table class="table tblBusquedaPacientesModal table-striped">
+           	    		<thead>
+           	    			<tr>
+           	    				<th>#</th>
+           	    				<th>Nombre</th>
+           	    				<th>Dirección</th>
+           	    				<th>Teléfono</th>
+           	    			</tr>
+           	    		</thead>
+						<tbody></tbody>
+           	    	</table>
+       	    	</div>
+   	        	<div class="modal-footer">
+                	<a href="#" class="btn" data-dismiss="modal">Cerrar</a>
+           		</div>
+       		</div>
+       		</br></br>
+		</section>
+			<br><br>
+			<section>
 				<legend>Ajuste de Lasik</legend>
+				<div class="row">
+					<table class="span8">
+						<tbody>
+							<tr>
+								<th style="text-align:center;">Nombre</th>
+								<th><input type="text" class="input-xxlarge" id="nombre" disabled="disabled"></th>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 				<div class="row">
 					<table class="span8">
 						<tbody>
@@ -93,344 +147,15 @@
 						</tbody>
 					</table>
 				</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-				<!--<div class="row">
-					<table class="span8">
-						<thead>
-							<tr>
-								<th></th>
-								<th style="text-align:center;">Ojo Derecho</th>
-								<th style="text-align:center;">Ojo Izquierdo</th>
-								<th></th>
-								<th style="text-align:center;">Eje</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td class="span2">Queratometría</td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtQueratometriaOD"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtQueratometriaOI"></td>
-								<td>X</td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtQueratometriaEje"></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<br><br>
-				<div class="row">
-					<table class="span8">
-						<tbody>
-							<tr>
-								<th>Ajuste 20%</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste20Esfera" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste20Cilindro" disabled="disabled"></td>
-								<td>X</td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste20Eje" disabled="disabled"></td>
-							</tr>
-							<tr>
-								<th>Ajuste 19%</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste19Esfera" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste19Cilindro" disabled="disabled"></td>
-								<td>X</td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste19Eje" disabled="disabled"></td>
-							</tr>
-							<tr>
-								<th>Ajuste 18%</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste18Esfera" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste18Cilindro" disabled="disabled"></td>
-								<td>X</td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste18Eje" disabled="disabled"></td>
-							</tr>
-							<tr>
-								<th>Ajuste 17%</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste17Esfera" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste17Cilindro" disabled="disabled"></td>
-								<td>X</td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste17Eje" disabled="disabled"></td>
-							</tr>
-							<tr>
-								<th>Ajuste 16%</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste16Esfera" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste16Cilindro" disabled="disabled"></td>
-								<td>X</td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste16Eje" disabled="disabled"></td>
-							</tr>
-							<tr>
-								<th>Ajuste 15%</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste15Esfera" disabled="disabled"</td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste15Cilindro" disabled="disabled"</td>
-								<td>X</td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjuste15Eje" disabled="disabled"</td>
-							</tr>				
-						</tbody>
-					</table>
-					<div class="span4">
-					  	<b>Promedio Queratometrías: </b><input type="text" class="input-small" id="txtPromedioQueratometrias" disabled="disabled">
-					</div>
-				</div>
-			</section>
-
-			<section id="miopia">
-				<div>
-					<legend>CAP Miopia</legend>
-				</div>
-				<div class="row">
-					<table class="span10">
-						<thead>
-							<tr>
-								<th></th>
-								<th style="text-align:center;">Esfera</th>
-								<th style="text-align:center;">Cilindro</th>
-								<th style="text-align:center;">Eje</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<th>Refracción</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtRefraccionEsfera2"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtRefraccionCilindro2"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtRefraccionEje2"></td>
-							</tr>
-							<tr>
-								<th>Distancia Vertex</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtDistanciaVertex"></td>
-								<td></td>
-								<td></td>
-								<th style="text-align:center;">Porcentaje Ajuste de Lasik</th>
-							</tr>
-							<tr>
-								<th>Ajuste a Plano Corneal</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjustePlanoEsfera" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjustePlanoCilindro" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjustePlanoEje" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtPorcentajeAjusteLasik" /></td>
-							</tr>
-							<tr>
-								<th>Ajuste de Lasik</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjusteLasikEsfera" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjusteLasikCilindro" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjusteLasikEje" disabled="disabled"></td>
-							</tr>
-							<tr>
-								<th>Ajuste de Cilindro</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjusteCilindroEsfera" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjusteCilindroCilindro" disabled="disabled"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAjusteCilindroEje" disabled="disabled"></td>
-							</tr>				
-						</tbody>
-					</table>
-				</div>
-				<div class="row">
-					<table class="span7 offset1">
-						<thead>
-							<tr>
-								<th colspan="3">Queratometrías</th>
-							</tr>
-							<tr>
-								<th>OD</th>
-								<th>OI</th>
-								<th>Eje</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td><input type="text" class="input-small" class="input-small" id="txtQueratometriaOD2"></td>
-								<td><input type="text" class="input-small" class="input-small" id="txtQueratometriaOI2"></td>
-								<td><input type="text" class="input-small" class="input-small" id="txtQueratometriaEje2"></td>
-							</tr>
-							<tr>
-								<td colspan="3" style="text-align:center;"><b>Promedio de Queratometrías:</b><input type="text" class="input-small" id="txtPromedioQueratometrias2" disabled="disabled"></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			
-				<div class="row">
-					<h5>Resultado:</h5>
-					<table class="span6">
-						<thead>
-							<tr>
-								<th>Esfera</th>
-								<th>Cilindro</th>
-								<th>Eje</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td><input type="text" class="input-small" id="txtEsfera" disabled="disabled"></td>
-								<td><input type="text" class="input-small" id="txtCilindro" disabled="disabled"></td>
-								<td><input type="text" class="input-small" id="txtEje" disabled="disabled"></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div class="row">
-					<h5>Reducción de la Esfera por Cilindro Puro:</h5>
-				
-				<table class="span6">
-					<thead>
-						<tr>
-							<th>Cilindro Puro</th>
-							<th>Esfera Ajustada</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td><input type="text" class="input-small" id="txtCilindroPuro"></td>
-							<td><input type="text" class="input-small" id="txtEsferaAjustada" disabled="disabled"></td>
-						</tr>
-					</tbody>
-				</table>
-				</div>
-			</section>
-
-			<section id="hipermetropia">
-				<legend>Hipermetropia y Astigmatismo</legend>
-				<div class="row">
-					<table class="span8">
-						<thead>
-							<tr>
-								<th></th>
-								<th>Esfera</th>
-								<th>Cilindro</th>
-								<th>Eje</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<th>Refracción</th>
-								<td><input type="text" class="input-small" id="txtRefraccionEsfera3"></td>
-								<td><input type="text" class="input-small" id="txtRefraccionCilindro3"></td>
-								<td><input type="text" class="input-small" id="txtRefraccionEje3"></td>
-							</tr>
-							<tr>
-								<th>Distancia al Vertex</th>
-								<td><input type="text" class="input-small" id="txtDistanciaVertex3"></td>
-							</tr>
-							<tr>
-								<th>Tratamiento</th>
-								<td><input type="text" class="input-small" id="txtTratamientoEsfera3" disabled="disabled"></td>
-								<td><input type="text" class="input-small" id="txtTratamientoCilindro3" disabled="disabled"></td>
-								<td><input type="text" class="input-small" id="txtTratamientoEje3" disabled="disabled"></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div class="row">
-					<table class="span7 offset1">
-						<thead>
-							<tr>
-							<th colspan="3">Queratometrías</th>
-						</tr>
-							<tr>
-								<th>OD</th>
-								<th>OI</th>
-								<th>Eje</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td><input type="text" class="input-small" id="txtQueratometriaOD3"></td>
-								<td><input type="text" class="input-small" id="txtQueratometriaOI3"></td>
-								<td><input type="text" class="input-small" id="txtQueratometriaEje3"></td>
-							</tr>
-							<tr>
-								<td colspan="3" style="text-align:center;"><b>Promedio de Queratometrías:</b><input type="text" class="input-small" id="txtPromedio3" disabled="disabled"></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				</br></br>
-			</section>
-
-			<section id="astigmatismo">
-				<legend>Astigmatismo Mixto</legend>
-				<div class="row">
-					<table class="span8">
-						<thead>
-							<tr>
-								<th></th>
-								<th>Esfera</th>
-								<th>Cilindro</th>
-								<th>Eje</th>
-							</tr>				
-						</thead> 
-						<tbody>
-							<tr>
-								<th>Refracción</th>
-								<td><input type="text" class="input-small" id="txtRefraccionEsfera4"></td>
-								<td><input type="text" class="input-small" id="txtRefraccionCilindro4"></td>
-								<td><input type="text" class="input-small" id="txtRefraccionEje4"></td>
-							</tr>
-							<tr>
-								<th>Transponer</th>
-								<td><input type="text" class="input-small" id="txtTransponerEsfera4" disabled="disabled"></td>
-								<td><input type="text" class="input-small" id="txtTransponerCilindro4" disabled="disabled"></td>
-								<td><input type="text" class="input-small" id="txtTransponerEje4" disabled="disabled"></td>
-							</tr>
-						</tbody>
-					</table>
-					<table class="span5">
-						<thead>
-							<tr>
-								<th></th>
-								<th>Poder</th>
-								<th>Eje</th>
-							</tr>				
-						</thead>
-						<tbody>
-							<tr>
-								<th>Cilindro Hipermetropico</th>
-								<td><input type="text" class="input-small" id="txtCilindroHipermetropicoPoder" disabled="disabled"></td>
-								<td><input type="text" class="input-small" id="txtCilindroHipermetropicoEje" disabled="disabled"></td>
-							</tr>
-							<tr>
-								<th>Cilindro Miopico</th>
-								<td><input type="text" class="input-small" id="txtCilindroMiopicoPoder" disabled="disabled"></td>
-								<td><input type="text" class="input-small" id="txtCilindroMiopicoEje" disabled="disabled"></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div class="row">
-					<h5>Queratometrias</h5>
-					<table class="span4">
-						<tbody>
-							<tr>
-								<th>OD</th>
-								<td><input type="text" class="input-small" id="txtQueratometriasOD4"></td>
-							</tr>
-							<tr>
-								<th>OI</th>
-								<td><input type="text" class="input-small" id="txtQueratometriasOI4"></td>
-							</tr>
-							<tr>
-								<th>Eje</th>
-								<td><input type="text" class="input-small" id="txtQueratometriasEje4"></td>
-							</tr>
-							<tr>
-								<th>Promedio</th>
-								<td><input type="text" class="input-small" id="txtQueratometriasPromedio4" disabled="disabled"></td>
-							</tr>				
-						</tbody>
-					</table>
-				</div>
-				</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
-			</section>-->
 		</div>
+		<!-- start: Button -->
+		<div class="control-group" id="btn_box">
+		  	<label class="control-label"></label>
+		  	<div class="controls">
+		    	<input value="Guardar" class="btn btn-large btn-primary" type="submit" id="btnGuardarExpedientePaciente">
+		  	</div>
+		</div>
+		<!-- end: button-->
 @stop
 
 
--- public/js/OperacionLaser2.js
+++ public/js/OperacionLaser2.js
@@ -30,6 +30,7 @@ function inicio() //Inicio del documento
 		change:CalculoLasik,
 		keyup:CalculoLasik
 	});
+	$("#btnBuscarPaciente").on("click",buscarPaciente);
 	
 }
 function CalculoLasik() // Funcion donde se calcula todo 
@@ -231,4 +232,57 @@ function CalculoLasik() // Funcion donde se calcula todo
 		$("#txtVal9").val(D9.toFixed(2));
 		$("#txtVal11").val(D10.toFixed(2));
 	console.log(J2);
+}
+function buscarPaciente(){ //Busca los pacientes segun criterio de busqueda y los muestra en una tabla
+	var contenido = $('.tblBusquedaPacientesModal tbody');
+    $.ajax({
+            data:  'buscarPaciente=' + $('#txtBuscarPaciente').val(),
+            url:   'buscarPaciente',
+            type:  'post',
+            beforeSend: function () {
+            	contenido.html('Buscando...');
+            },
+            success:  function (response) {
+            	contenido.html('');
+            	$.each(response.Pacientes, function(i,elemento){
+            		$('<tr><td>'+elemento.IdPaciente+'</td><td>'+elemento.Nombre+'</td><td>'+elemento.Domicilio+'</td><td>'+elemento.Telefono+'</td></tr>').appendTo(contenido);
+            	});
+            }
+    });
+    return false;
+}
+function tablaBusquedaPacientesModal(){ //Funcion que permite interactuar con la tabla de busqueda de los pacientes
+    $('#mdlBuscarPaciente').on('shown', function(){
+        $("#txtBuscarPaciente").focus();
+    });
+	$('.tblBusquedaPacientesModal tbody').on('mouseover', 'tr', function(event) { //Toma el evento mouseover en funcion live para que el tr seleccionado cambie de color al igual que el cursor
+		$(this).parent().parent().removeClass("table-striped");
+	    $(this).css({"color":"white","background-color":"#2E9FFC","cursor":"pointer"});
+    });
+
+    $('.tblBusquedaPacientesModal tbody').on('mouseout', 'tr', function(event) { // evento mouseout que elimina los estilos (background de todo el tr) y agrega el estilo sebra a la tabla
+		$(this).parent().parent().addClass("table-striped");
+        $(this).removeAttr("style");
+    });
+
+    $('.tblBusquedaPacientesModal tbody').on('click', 'tr', function(event) { // permite agregar a la tabla de personas principal el registro que se seleccione.              
+		idPaciente = $(this).children('td')[0].innerText;
+        buscarPacienteConId(idPaciente);
+        $(".close").click();
+    });
+}
+function buscarPacienteConId(idPaciente){ //Funcion que toma el id del paciente seleccionado en la tabla de la busqueda y carga los datos en los controles del formulario
+    var contenido = $('.tblBusquedaPacientesModal tbody');
+	$.ajax({
+            data:  'idPaciente=' + idPaciente,
+            url:   'buscarPacientePorId',
+            type:  'post',
+            success:  function (response) {               
+            	$.each(response.Paciente, function(i,elemento){
+            		// Datos Generales del Paciente
+            		$("#nombre").val(elemento.Nombre);
+            		
+            	});
+            }
+    });
 }
