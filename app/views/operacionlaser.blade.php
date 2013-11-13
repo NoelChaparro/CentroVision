e6488b864156bc78a8aaddfb80fac9865e402b6e
joseluisordonez <joseluisordonezmacias@hotmail.com>
2013-11-12 19:27:10 -0700

    se agrego otro formulario para ojo izq

--- app/views/operacionlaser.blade.php
+++ app/views/operacionlaser.blade.php
@@ -4,7 +4,6 @@
 @stop
 
 @section('Contenido')	 
-		<div>
 			</br></br>
 			<section id="lasik">
 			<div class="row offset1">
@@ -52,90 +51,154 @@
 			<br><br>
 				<legend>Ajuste de Lasik</legend>
 				<div class="row">
-					<table class="span8">
+					<table class="span10">
 						<tbody>
 							<tr>
 								<th style="text-align:center;">Nombre</th>
 								<th><input type="text" class="input-xxlarge" id="nombre" disabled="disabled"></th>
 								<th style="text-align:center;">Fecha</th>
 								<th><input type="text" class="input-small"  name="fecha" id="fecha" disabled="disabled"></th>
+								<th style="text-align:center;">Edad</th>
+								<th><input type="text" class="input-mini" id="txtEdad"></th>
 							</tr>
 						</tbody>
 					</table>
 				</div>
+				</br></br>
 				<form action="" id="frmOperacionLaser">
 				<div class="row">
-					<table class="span8">
-						<tbody>
-							<tr>
-								<th style="text-align:center;">Edad</th>
-								<th><input type="text" class="input-small" id="txtEdad"></th>
-								<th style="text-align:center;">Hiper</th>
-								<th><input type="text" class="input-small" id="txtHiper"></th>
-							</tr>
-							<tr>
-								<th style="text-align:center;">KH</th>
-								<th><input type="text" class="input-small" id="txtKH"></th>
-								<th style="text-align:center;">KV</th>
-								<th><input type="text" class="input-small" id="txtKV"></th>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div class="row">
-					<table class="span8">
-						<thead>
-							<tr>
-								<th style="text-align:center;">Esfera</th>
-								<th style="text-align:center;">Cilindro</th>
-								<th style="text-align:center;">Eje</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtRefraccionEsfera"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtRefraccionCilindro"></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtRefraccionEje"></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div class="row">
-					<table class="span8">
-						<tbody>
-							<tr>
-								<th style="text-align:right;">Anillo</th>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtAnillo" disabled="disabled"></td>	
-								<td></td>
-								<td></td>
-							</tr>
-							<tr>
-								<td></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal1" disabled="disabled"></td>							
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal2" disabled="disabled"></td>							
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal3" disabled="disabled"></td>							
-							</tr>
-							<tr>
-								<td></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal4" disabled="disabled"></td>							
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal5" disabled="disabled"></td>							
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal6" disabled="disabled"></td>							
-							</tr>
-							<tr>
-								<td></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal7" disabled="disabled"></td>							
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal8" disabled="disabled"></td>							
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal9" disabled="disabled"></td>							
-							</tr>
-							<tr>
-								<td></td>
-								<td></td>
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal10" disabled="disabled"></td>							
-								<td style="text-align:center;"><input type="text" class="input-small" id="txtVal11" disabled="disabled"></td>							
-							</tr>
-						</tbody>
-					</table>
-				</div>
+					<div class="span6">
+						<div class="row">
+							<div class="span5">
+								<H4>Ojo Derecho</H4>
+							</div>
+						</div>
+						<div class="row">
+							<table class="span7">
+								<tbody>
+									<tr>
+										<th style="text-align:center;">Hiper</th>
+										<th><input type="text" class="input-small" id="txtHiper"></th>
+										<th style="text-align:center;">KH</th>
+										<th><input type="text" class="input-small" id="txtKH"></th>
+										<th style="text-align:center;">KV</th>
+										<th><input type="text" class="input-small" id="txtKV"></th>
+									</tr>
+									<tr>
+										<th style="text-align:center;">Esfera</th>
+										<th><input type="text" class="input-small" id="txtRefraccionEsfera"></th>
+										<th style="text-align:center;">Cilindro</th>
+										<th><input type="text" class="input-small" id="txtRefraccionCilindro"></th>
+										<th style="text-align:center;">Eje</th>
+										<th><input type="text" class="input-small" id="txtRefraccionEje"></th>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<div class="row">
+							<table class="span7">
+								<tbody>
+									<tr>
+										<th style="text-align:right;">Anillo</th>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtAnillo" disabled="disabled"></td>	
+										<td></td>
+										<td></td>
+									</tr>
+									<tr>
+										<td></td>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal1" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal2" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal3" disabled="disabled"></td>							
+									</tr>
+									<tr>
+										<td></td>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal4" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal5" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal6" disabled="disabled"></td>							
+									</tr>
+									<tr>
+										<td></td>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal7" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal8" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal9" disabled="disabled"></td>							
+									</tr>
+									<tr>
+										<td></td>
+										<td></td>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal10" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal11" disabled="disabled"></td>							
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<div class="offset2 span6">
+						<div class="row">
+							<div class="span5">
+								<H4>Ojo Izquierdo</H4>
+							</div>
+						</div>
+						<div class="row">
+							<table class="span7">
+								<tbody>
+									<tr>
+										<th style="text-align:center;">Hiper</th>
+										<th><input type="text" class="input-small" id="txtHiperI"></th>
+										<th style="text-align:center;">KH</th>
+										<th><input type="text" class="input-small" id="txtKHI"></th>
+										<th style="text-align:center;">KV</th>
+										<th><input type="text" class="input-small" id="txtKVI"></th>
+									</tr>
+									<tr>
+										<th style="text-align:center;">Esfera</th>
+										<th><input type="text" class="input-small" id="txtRefraccionEsferaI"></th>
+										<th style="text-align:center;">Cilindro</th>
+										<th><input type="text" class="input-small" id="txtRefraccionCilindroI"></th>
+										<th style="text-align:center;">Eje</th>
+										<th><input type="text" class="input-small" id="txtRefraccionEjeI"></th>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<div class="row">
+							<table class="span7">
+								<tbody>
+									<tr>
+										<th style="text-align:right;">Anillo</th>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtAnilloI" disabled="disabled"></td>	
+										<td></td>
+										<td></td>
+									</tr>
+									<tr>
+										<td></td>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal1I" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal2I" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal3I" disabled="disabled"></td>							
+									</tr>
+									<tr>
+										<td></td>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal4I" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal5I" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal6I" disabled="disabled"></td>							
+									</tr>
+									<tr>
+										<td></td>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal7I" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal8I" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal9I" disabled="disabled"></td>							
+									</tr>
+									<tr>
+										<td></td>
+										<td></td>
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal10I" disabled="disabled"></td>							
+										<td style="text-align:center;"><input type="text" class="input-small" id="txtVal11I" disabled="disabled"></td>							
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>	
+				
 				</form>
 		</div>
 		<!-- start: Button -->
